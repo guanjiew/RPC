1ddfd55639cfcb2fd5d2da41677155bf213dd72a
Jacky Wang <jackyguanjie.wang@gmail.com>
2023-05-18 05:08:59 -0400

    update

---
+++ script.js
@@ -0,0 +1,143 @@
+// Set the base URL for your server
+axios.defaults.baseURL = "http://localhost:3000"; // Replace with your server URL
+
+// Set the CORS headers
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.common["Access-Control-Allow-Methods"] =
+  "GET, POST, OPTIONS";
+
+function makeChoice(playerChoice) {
+  var buttons = document.querySelectorAll(".choices button");
+  buttons.forEach(function (button) {
+    button.classList.remove("highlight");
+  });
+
+  var computerChoice = getComputerChoice();
+  var result = determineWinner(playerChoice, computerChoice);
+
+  var resultMessage = document.querySelector(".result-message");
+  resultMessage.innerHTML =
+    "<p>You chose " +
+    playerChoice +
+    ".<br>The computer chose " +
+    computerChoice +
+    ".</p><p>" +
+    result +
+    "</p>";
+
+  // Log the game result to the server-side database
+  logGameResult(playerChoice, computerChoice, result);
+}
+
+function getComputerChoice() {
+  var choices = ["rock", "paper", "scissors"];
+  var randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
+
+function determineWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "It's a tie!";
+  }
+
+  if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
+function logGameResult(playerChoice, computerChoice, result) {
+  axios
+    .post("/record", {
+      playerChoice: playerChoice,
+      computerChoice: computerChoice,
+      result: result,
+    })
+    .then(function (response) {
+      console.log("Game result logged successfully!");
+    })
+    .catch(function (error) {
+      console.error("An error occurred while logging the game result:", error);
+    });
+}
+
+var currentPage = 1;
+var pageSize = 5; // Number of results per page
+
+// Function to fetch game results from the server and update the table
+function fetchGameResults(page) {
+  axios
+    .get("/game-results", {
+      params: {
+        page: page,
+        pageSize: pageSize,
+      },
+    })
+    .then((response) => {
+      var tableBody = document.querySelector("#game-results-table tbody");
+      tableBody.innerHTML = "";
+
+      console.log("Game results:", response.data.results);
+
+      response.data.results.forEach((result) => {
+        var row = document.createElement("tr");
+        row.innerHTML = `
+              <td>${result.player1}</td>
+              <td>${result.player2}</td>
+              <td>${result.result}</td>
+            `;
+        tableBody.appendChild(row);
+      });
+
+      updatePagination(response.data.totalPages);
+    })
+    .catch((error) => {
+      console.error("Error fetching game results:", error);
+    });
+}
+
+// Function to update the pagination buttons
+function updatePagination(totalPages) {
+  var pagination = document.querySelector(".pagination");
+  pagination.innerHTML = "";
+
+  var prevButton = document.createElement("button");
+  prevButton.textContent = "Prev";
+  prevButton.addEventListener("click", () => goToPage("prev"));
+  pagination.appendChild(prevButton);
+
+  for (var i = 1; i <= totalPages; i++) {
+    var pageButton = document.createElement("button");
+    pageButton.textContent = i;
+    pageButton.classList.toggle("active", i === currentPage);
+    pageButton.addEventListener("click", () => goToPage(i));
+    pagination.appendChild(pageButton);
+  }
+
+  var nextButton = document.createElement("button");
+  nextButton.textContent = "Next";
+  nextButton.addEventListener("click", () => goToPage("next"));
+  pagination.appendChild(nextButton);
+}
+
+// Function to navigate to a specific page or prev/next page
+function goToPage(page) {
+  if (page === "prev" && currentPage > 1) {
+    currentPage--;
+  } else if (page === "next") {
+    currentPage++;
+  } else {
+    currentPage = page;
+  }
+  fetchGameResults(currentPage);
+}
+
+// Call fetchGameResults with the initial page when the page loads
+window.addEventListener("load", () => fetchGameResults(currentPage));
+// Call fetchGameResults when the page loads
+window.addEventListener("load", fetchGameResults);
--- server.js
+++ server.js
@@ -9,14 +9,29 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const db = new sqlite3.Database("game_results.db");
+const db = new sqlite3.Database("game_results.db", (err) => {
+  if (err) {
+    console.error("Error opening database:", err.message);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
 
-db.run(`CREATE TABLE IF NOT EXISTS games (
+db.run(
+  `CREATE TABLE IF NOT EXISTS games (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   player1 TEXT,
   player2 TEXT,
   result TEXT
-)`);
+)`,
+  (err) => {
+    if (err) {
+      console.error("Error creating games table:", err.message);
+    } else {
+      console.log("Games table created successfully.");
+    }
+  }
+);
 
 // Handle POST requests to record game results
 app.post("/record", (req, res) => {
@@ -38,7 +53,50 @@ app.post("/record", (req, res) => {
   );
 });
 
+// Endpoint to retrieve game results with pagination
+app.get("/game-results", (req, res) => {
+  const { page, pageSize } = req.query;
+
+  // Prepare the pagination parameters
+  const limit = parseInt(pageSize);
+  const offset = (parseInt(page) - 1) * parseInt(pageSize) || 0;
+
+  console.log("Retrieving game results with pagination:", limit, offset);
+
+  // Query the database for game results with pagination
+  const query = `SELECT * FROM games LIMIT ? OFFSET ?`;
+  db.all(query, [limit, offset], (err, rows) => {
+    if (err) {
+      console.error("Error retrieving game results:", err);
+      res.status(500).send("Error retrieving game results");
+      return;
+    }
+
+    // Query the database for the total number of game results
+    const countQuery = `SELECT COUNT(*) AS count FROM games`;
+    db.get(countQuery, (countErr, { count }) => {
+      if (countErr) {
+        console.error("Error retrieving game results count:", countErr);
+        res.status(500).send("Error retrieving game results count");
+        return;
+      }
+
+      // Calculate the total number of pages
+      const totalPages = Math.ceil(count / limit);
+
+      // Prepare the response object
+      const response = {
+        results: rows,
+        totalPages,
+      };
+
+      // Send the response
+      res.json(response);
+    });
+  });
+});
+
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
